61ff7f418382d460a1ab4de58911e210742cac04
Sebastian Gutierrez <sebagutierez@gmail.com>
2022-12-13 10:17:18 -0300

    correccion axios route

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
-// const axios = require("axios");
 
 const dotenv = require("dotenv").config();
 
